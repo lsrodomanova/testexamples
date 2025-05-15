a4d841d3d652e4a920f470fed972572c47226b33
Liuba <lrodomanova1710@gmail.com>
2025-05-15 09:59:51 +0700

    tests api

--- tests/test_api.py
+++ tests/test_api.py
@@ -32,3 +32,39 @@ def test_create_item():
     },
         )
     assert response.ok
+    assert response.status == 200
+
+@allure.feature('Поиск товара')
+@allure.story('Поиск по названию')
+@allure.title('Поиск аозай')
+def test_search_item():
+    with sync_playwright() as p:
+        # Это запустит новый браузер, создаст контекст и страницу. При выполнении HTTP
+        # запросов с использованием внутреннего APIRequestContext (например, `context.request` или `page.request`)
+        # cookies будут автоматически установлены на странице браузера и наоборот.
+        #browser = p.chromium.launch()
+        #context = browser.new_context()
+        #api_request_context = context.request
+        #page = context.new_page()
+
+        # В качестве альтернативы вы можете создать APIRequestContext вручную без привязки к контексту браузера:
+        api_request_context = p.request.new_context(base_url="http://shop.bugred.ru")
+
+        # Найти товар.
+        response = api_request_context.post(
+            "/api/items/search/",
+            headers={
+                "Accept": "application/json"
+            },
+            data={
+                "query": "Аозай"
+                },
+        )
+        assert response.ok
+        assert response.status == 200
+
+        data = response.json()
+        items = data.get("result", [])
+
+        assert any(item["name"] == "Аозай" for item in items), "Товар 'Аозай' не найден"
+
